4864cc94d11c262a864d97dbea1cc81a9c6dc775
Pablo Hernandez Cepeda <Pablo Hernandez Cepeda>
2016-08-26 03:18:03 -0500

    more

--- test.py
+++ test.py
@@ -4,6 +4,13 @@ counter = 100
 miles = 1000.0
 name = "John"
 
+lista = ["john", 3, 4, "lolson"]
+small_list = ["hello", "world"]
+
 print(counter)
 print(miles)
 print(name)
+print(lista[2])
+print(lista[1:])
+print(lista + small_list)
+print(lista * 3)
